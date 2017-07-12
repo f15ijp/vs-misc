d3ea0e95a19babc80a44c7818e58376e6747f410
U-MILJODATA\jonas <jonas@jonas-tp.miljodata.local>
2017-07-12 14:29:00 +0200

    Added a simple MSTest project

---
+++ MSTest.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTest.Test
+{
+	[TestClass]
+	public class MSTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Assert.IsTrue(true);
+		}
+	}
+}
