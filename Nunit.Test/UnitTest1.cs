635e1d25113df4bc5e90c444070df204cee7394f
Jonas Petersson <jonas@example.com>
2017-07-12 14:39:36 +0200

    Added a simple NUnit test project
     Incheckare: jonas

---
+++ Nunit.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Nunit.Test
+{
+	[TestFixture]
+	public class Nunit
+	{
+		[TestCase]
+		public void TestMethod1()
+		{
+			Assert.That(true, Is.True);
+		}
+	}
+}
