c32e4bc4edea1faa0b6ea80dcfe080e8c0d7d429
Jonas Petersson <f15ijp@users.noreply.github.com>
2017-07-14 15:01:17 +0200

    Added simple FizzBuzz

---
+++ Misc/Fizzbuzz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misc
+{
+    public class Fizzbuzz
+    {
+
+		public static string FizzBuzz(int number)
+		{
+			if (number <= 0)
+			{
+				throw new ArgumentException();
+			}
+
+			string retVal = string.Empty;
+			if (number % 15 == 0)
+			{
+				retVal = "FizzBuzz";
+			}
+			else if (number % 3 == 0)
+			{
+				retVal = "Fizz";
+			}
+			else if (number % 5 == 0)
+			{
+				retVal = "Buzz";
+			}
+			else
+			{
+				retVal = number.ToString();
+			}
+			return retVal;
+		}
+
+	}
+}
