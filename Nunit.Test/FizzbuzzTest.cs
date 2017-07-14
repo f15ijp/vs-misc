f66ac7111484de2b1962835716475d90f08ce076
jonas <jonas@example.com>
2017-07-14 15:38:57 +0200

    Better test coverage.

--- Nunit.Test/FizzbuzzTest.cs
+++ Nunit.Test/FizzbuzzTest.cs
@@ -8,6 +8,9 @@ namespace Nunit.Test
 	{
 		[TestCase(1, ExpectedResult = "1")]
 		[TestCase(2, ExpectedResult = "2")]
+		[TestCase(3, ExpectedResult = "Fizz")]
+		[TestCase(5, ExpectedResult = "Buzz")]
+		[TestCase(15, ExpectedResult = "FizzBuzz")]
 		public string testCases(int Input)
 		{
 
