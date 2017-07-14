b72288ecd5189bf169ebe2580fd884fe5bd2a6e8
jonas <jonas@example.com>
2017-07-14 15:08:03 +0200

    Added first TestCase.

--- Nunit.Test/FizzbuzzTest.cs
+++ Nunit.Test/FizzbuzzTest.cs
@@ -6,6 +6,13 @@ namespace Nunit.Test
 	[TestFixture]
 	public class FizzbuzzTest
 	{
-		
+		[TestCase(1, ExpectedResult = "1")]
+		[TestCase(2, ExpectedResult = "2")]
+		public string testCases(int Input)
+		{
+
+			return Misc.Fizzbuzz.FizzBuzz(Input);
+
+		}
 	}
 }
