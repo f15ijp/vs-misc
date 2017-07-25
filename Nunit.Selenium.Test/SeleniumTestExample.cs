5fa66fa27dc1e0a2b09e41a67642827f66d8b8fd
Jonas Petersson <f15ijp@users.noreply.github.com>
2017-07-25 14:14:09 +0200

    Added a very Seleniun (Nunit) test project)

---
+++ Nunit.Selenium.Test/SeleniumTestExample.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Remote;
+
+namespace Nunit.Selenium.Test
+{
+
+	[TestFixture()]
+	public class SeleniumTestExample
+	{
+
+		[Test()]
+
+		public void OpenPageAndCheckUseragentString()
+		{
+
+			var options = new InternetExplorerOptions()
+			{
+				InitialBrowserUrl = "http://www.f15ijp.com/misc/ua.php",
+				IntroduceInstabilityByIgnoringProtectedModeSettings = true,
+				IgnoreZoomLevel = true,
+				EnableNativeEvents = false,
+				UnexpectedAlertBehavior = InternetExplorerUnexpectedAlertBehavior.Dismiss
+			};
+
+			IWebDriver driver = new InternetExplorerDriver(options);
+						
+			try
+			{
+
+				By locator = By.Id("useragent");
+
+				//Open URl
+				driver.Url = "http://www.f15ijp.com/misc/ua.php";
+				//Wait
+				WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 30));
+				wait.Until(ExpectedConditions.ElementIsVisible(locator));
+
+				//find an element om the page
+				IWebElement theElement = driver.FindElement(locator);
+
+				Assert.That(theElement.Text, Contains.Substring("trident").IgnoreCase.Or.Contains("internet").IgnoreCase.And.Contains("explorer").IgnoreCase);				
+			}
+			finally
+			{
+
+				//Even if many windows were opened - close all of them
+				foreach (string windowHandle in driver.WindowHandles.ToList())
+				{
+					driver.SwitchTo().Window(windowHandle);
+					driver.Close();
+
+				}
+
+			}
+
+		}
+
+	}
+}
