40b3692b7aae3b79b4a728ec53b4cae4feb98064
Jonas Petersson <f15ijp@users.noreply.github.com>
2017-08-01 12:24:07 +0200

    Added example of how to use SeleniumGrid

--- Nunit.Selenium.Test/SeleniumTestExample.cs
+++ Nunit.Selenium.Test/SeleniumTestExample.cs
@@ -14,6 +14,7 @@ namespace Nunit.Selenium.Test
 	{
 
 		[Test()]
+		[Ignore("Don't run in CI")]
 
 		public void OpenPageAndCheckUseragentString()
 		{
@@ -28,7 +29,7 @@ namespace Nunit.Selenium.Test
 			};
 
 			IWebDriver driver = new InternetExplorerDriver(options);
-						
+
 			try
 			{
 
@@ -43,7 +44,7 @@ namespace Nunit.Selenium.Test
 				//find an element om the page
 				IWebElement theElement = driver.FindElement(locator);
 
-				Assert.That(theElement.Text, Contains.Substring("trident").IgnoreCase.Or.Contains("internet").IgnoreCase.And.Contains("explorer").IgnoreCase);				
+				Assert.That(theElement.Text, Contains.Substring("trident").IgnoreCase.Or.Contains("internet").IgnoreCase.And.Contains("explorer").IgnoreCase);
 			}
 			finally
 			{
@@ -53,6 +54,60 @@ namespace Nunit.Selenium.Test
 			}
 
 		}
+
+		[Test()]
+		public void seleniumGrid()
+		{
+			string hubUrl = System.Configuration.ConfigurationManager.AppSettings["seleniumHub"];
+			if (string.IsNullOrEmpty(hubUrl))
+			{
+				hubUrl = "http://localhost:4444/wd/hub";
+			}
+
+			RemoteWebDriver driver = null;
+			try
+			{
+				DesiredCapabilities browserCapabilites = DesiredCapabilities.InternetExplorer();
+				browserCapabilites.SetCapability("ignoreProtectedModeSettings", true);
+				browserCapabilites.SetCapability("ie.forceCreateProcessApi", true);
+				browserCapabilites.SetCapability("ie.browserCommandLineSwitches", "-private");
+
+				driver = new RemoteWebDriver(new Uri(hubUrl), browserCapabilites);
+
+				By locator = By.Id("useragent");
+
+
+				driver.Url = "http://www.f15ijp.com/misc/ua.php";
+
+				WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 30));
+				wait.Until(ExpectedConditions.ElementIsVisible(locator));
+
+
+				IWebElement theElement = driver.FindElement(locator);
+
+				if (driver.Capabilities.BrowserName.Equals("internet explorer"))
+				{
+					Assert.That(theElement.Text, Contains.Substring("trident").IgnoreCase.Or.Contains("internet").IgnoreCase.And.Contains("explorer").IgnoreCase);
+				}
+				else if (driver.Capabilities.BrowserName.Equals("opera"))
+				{
+					Assert.That(theElement.Text, Contains.Substring("OPR/").IgnoreCase);
+				}
+				else
+				{
+					Assert.That(theElement.Text, Contains.Substring(driver.Capabilities.BrowserName).IgnoreCase);
+				}
+
+			}
+			finally
+			{
+
+				driver.Quit();
+			}
+
+
+		}
+
 
 	}
 }
