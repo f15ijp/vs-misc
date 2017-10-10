5c130ac9df49ea02ff6e36968da605858b81ece0
Unknown <jonas.petersson@miljodata.se>
2017-10-10 14:33:27 +0200

    Added example of how to test types with Nunit

---
+++ Nunit.Test/Examples/TypeOfTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nunit.Test.Examples
+{
+	[TestFixture]
+	public class TypeOfTest
+	{
+		[Test]
+		public void TypeOfListOfInt()
+		{
+			List<int> theList = new List<int>();
+
+			Assert.That(theList, Is.TypeOf<List<int>>());
+		}
+	}
+}
