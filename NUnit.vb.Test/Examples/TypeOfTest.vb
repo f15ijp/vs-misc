966ad45e5c9ff9702d2edf70257b5667257828f4
f15ijp <jonas.petersson@miljodata.se>
2017-10-23 09:01:55 +0200

    Adds example with how to test against a type

---
+++ NUnit.vb.Test/Examples/TypeOfTest.vb
@@ -0,0 +1,21 @@
+﻿Option Explicit On
+
+Imports System
+Imports NUnit.Framework
+
+Namespace Examples
+
+	<TestFixture>
+	Public Class TypeOfTest
+
+		<Test>
+		Public Sub TypeOf_ListOfInt()
+
+			Dim testList AS New List(Of Integer)
+			Assert.That(testList, [Is].TypeOf(GetType(List(Of Integer))))
+
+		End Sub
+
+	End Class
+
+End Namespace
