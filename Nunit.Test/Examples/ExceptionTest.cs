9b562ab6a007aeb40c8a1125b88c5203b3a999c3
f15ijp <jonas.petersson@miljodata.se>
2017-10-29 11:22:47 +0100

    Add example of how to test for exceptions

---
+++ Nunit.Test/Examples/ExceptionTest.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Nunit.Test.Examples
+{
+	[TestFixture]
+	public class ExceptionTest
+	{
+
+		#region "Assert.Throws"
+
+		[Test]
+		public void Excpetion_Assert_Throws_TestVoid()
+		{
+			Assert.Throws<ArgumentException>(VoidThatThrows);
+		}
+
+		[Test]
+		public void Excpetion_Assert_Throws_TesFunction()
+		{
+			Assert.Throws<ArgumentException>(() => FunctionThatThrows());
+		}
+
+		#endregion
+
+		#region "Assert.That"
+
+		[Test]
+		public void Excpetion_Assert_That_TestVoid()
+		{
+			Assert.That(VoidThatThrows, Throws.ArgumentException);
+		}
+
+		[Test]
+		public void Excpetion_Assert_That_TestFunction()
+		{
+			Assert.That(FunctionThatThrows, Throws.ArgumentException);
+		}
+
+		#endregion
+
+		#region "Infrastructure for test"
+
+
+		private static void VoidThatThrows()
+		{
+			throw new ArgumentException("Throws for test");
+		}
+
+		private static string FunctionThatThrows()
+		{
+			throw new ArgumentException("Throws for test");
+		}
+
+		#endregion
+
+	}
+}
