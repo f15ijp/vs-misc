c3e2e30c733f90478fadfdd7932ba77c9fd1938b
f15ijp <jonas.petersson@miljodata.se>
2017-11-01 12:15:44 +0100

    Minor changes based on static code analysis (remove extra blank lines, removed unused imports)

--- Nunit.Test/Examples/ExceptionTest.cs
+++ Nunit.Test/Examples/ExceptionTest.cs
@@ -1,5 +1,5 @@
-﻿using NUnit.Framework;
-using System;
+﻿using System;
+using NUnit.Framework;
 
 namespace Nunit.Test.Examples
 {
--- Nunit.Test/Examples/TypeOfTest.cs
+++ Nunit.Test/Examples/TypeOfTest.cs
@@ -1,10 +1,5 @@
 ﻿using NUnit.Framework;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Metadata.W3cXsd2001;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nunit.Test.Examples
 {
