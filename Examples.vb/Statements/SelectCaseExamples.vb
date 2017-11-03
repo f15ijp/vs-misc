8b268dd95236d7a554522cc7b63bd394feacb9fb
f15ijp <jonas.petersson@miljodata.se>
2017-11-03 16:44:57 +0100

    Adds example of a Select Case for Types

---
+++ Examples.vb/Statements/SelectCaseExamples.vb
@@ -0,0 +1,31 @@
+﻿Option Explicit On
+
+Imports NUnit.Framework
+
+Namespace Statements
+
+	<TestFixture>
+	Public Class SelectCaseExamples
+
+		<Test>
+		Public Sub Select_Case_Types()
+
+			Assert.That(GetTypeName(Of String), [Is].EqualTo("string"))
+			Assert.That(GetTypeName(Of Integer), [Is].EqualTo("integer"))
+			
+		End Sub
+
+		Function GetTypeName(Of T)() As String
+			
+			Select Case GetType(T)
+				Case GetType(String)
+					Return "string"
+				Case GetType(Integer)
+					return "integer"
+			End Select
+			Throw New ArgumentOutOfRangeException
+		End Function
+
+	End Class
+
+End Namespace
