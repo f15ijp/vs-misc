8311f25d994d6158f28f7a17d130be864f4072ad
f15ijp <jonas.petersson@miljodata.se>
2017-11-03 16:27:13 +0100

    Adds example of how to convert from object to a Type (and account fro DBNull)

--- Examples.vb/SystemData/DataRowExamples.vb
+++ Examples.vb/SystemData/DataRowExamples.vb
@@ -5,12 +5,12 @@ Namespace SystemData
 
 	<TestFixture>
 	Public Class DataRowExamples
-		
-		<Test>Public Sub DataRow_Type_When_Null()
-			
+
+		<Test> Public Sub DataRow_Type_When_Null()
+
 			Dim drWithValue As DataRow = GetDataRow(True)
 			Dim drWithoutValue As DataRow = GetDataRow(False)
-			
+
 			Assert.That(drWithValue("string"), [Is].TypeOf(GetType(String)))
 			Assert.That(drWithoutValue("string"), [Is].TypeOf(GetType(DBNull)))
 
@@ -19,7 +19,35 @@ Namespace SystemData
 
 		End Sub
 
-		#Region "Infrastructure"
+		<Test> Public Sub DataRow_Type_With_Conversion_When_Null()
+
+			Dim drWithValue As DataRow = GetDataRow(True)
+			Dim drWithoutValue As DataRow = GetDataRow(False)
+
+			Assert.That(ConvertTo(Of String)(drWithValue("string")), [Is].TypeOf(GetType(String)))
+			Assert.That(ConvertTo(Of String)(drWithoutValue("string")), [Is].Null)
+
+			Assert.That(ConvertTo(Of Object)(drWithValue("object")), [Is].TypeOf(GetType(Object)))
+			Assert.That(ConvertTo(Of Object)(drWithoutValue("object")), [Is].Null)
+
+			Assert.That(ConvertTo(Of Integer)(drWithValue("integer")), [Is].TypeOf(GetType(Integer)))
+			Assert.That(ConvertTo(Of Integer)(drWithoutValue("integer")), [Is].TypeOf(GetType(Integer)))
+
+		End Sub
+
+		Shared Function ConvertTo(Of T)(ByVal value As Object) As T
+			If TypeOf (value) Is DBNull Then
+				Return CType(Nothing, T)
+			End If
+
+			Try
+				Return CType(value, T)
+			Catch ex As InvalidCastException
+				Return CType(Nothing, T)
+			End Try
+		End Function
+
+#Region "Infrastructure"
 
 		Public Function GetDataRow(popluateWithValues As Boolean) As DataRow
 
@@ -27,6 +55,7 @@ Namespace SystemData
 			If (popluateWithValues) Then
 				dr("string") = String.Empty
 				dr("object") = New Object
+				dr("integer") = 1
 			End If
 
 			Return dr
@@ -38,6 +67,7 @@ Namespace SystemData
 			Dim dt As New DataTable()
 			dt.Columns.Add(New DataColumn("string", GetType(String)))
 			dt.Columns.Add(New DataColumn("object", GetType(Object)))
+			dt.Columns.Add(New DataColumn("integer", GetType(Integer)))
 			dt.Columns.Add(New DataColumn("null", GetType(Object)))
 
 			dt.AcceptChanges()
@@ -45,7 +75,7 @@ Namespace SystemData
 
 		End Function
 
-		#End Region
+#End Region
 
 	End Class
 
