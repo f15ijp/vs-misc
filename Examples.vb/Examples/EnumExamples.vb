a3646b3bc5a63378259805714b15e9e3f35cfa5f
f15ijp <jonas.petersson@miljodata.se>
2017-11-30 10:15:29 +0100

    Adds example of how to get a random enum

---
+++ Examples.vb/Examples/EnumExamples.vb
@@ -0,0 +1,23 @@
+﻿Imports NUnit.Framework
+
+Namespace Examples
+	Public Class EnumExamples
+
+		Enum ExampleEnum
+			EnumOne
+			EnumTwo
+		End Enum
+
+		Public Shared Function GetRandomEnum(Of T)() As T
+
+			Dim v As T() = [Enum].GetValues(GetType(T))
+			Return CType(v.GetValue(New Random().[Next](v.Length)), T)
+
+		End Function
+
+		<Test> Public Sub GetRandomEnum_Example()
+			Assert.That(GetRandomEnum(Of ExampleEnum)(), [Is].TypeOf(GetType(ExampleEnum)))
+		End Sub
+
+	End Class
+End NameSpace
