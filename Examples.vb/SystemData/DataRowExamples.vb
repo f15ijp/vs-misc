006f085a03bdb11b8ecd482700a05f8347ca8b13
f15ijp <f15ijp@users.noreply.github.com>
2018-02-01 19:21:04 +0100

    More DataRow conversion examples

--- Examples.vb/SystemData/DataRowExamples.vb
+++ Examples.vb/SystemData/DataRowExamples.vb
@@ -19,23 +19,23 @@ Namespace SystemData
 
 		End Sub
 
-		<Test> Public Sub DataRow_Type_With_Conversion_When_Null()
+		<Test> Public Sub DataRow_Type_With_CType_Conversion_When_Null()
 
 			Dim drWithValue As DataRow = GetDataRow(True)
 			Dim drWithoutValue As DataRow = GetDataRow(False)
 
-			Assert.That(ConvertTo(Of String)(drWithValue("string")), [Is].TypeOf(GetType(String)))
-			Assert.That(ConvertTo(Of String)(drWithoutValue("string")), [Is].Null)
+			Assert.That(ConvertByCtype(Of String)(drWithValue("string")), [Is].TypeOf(GetType(String)))
+			Assert.That(ConvertByCtype(Of String)(drWithoutValue("string")), [Is].Null)
 
-			Assert.That(ConvertTo(Of Object)(drWithValue("object")), [Is].TypeOf(GetType(Object)))
-			Assert.That(ConvertTo(Of Object)(drWithoutValue("object")), [Is].Null)
+			Assert.That(ConvertByCtype(Of Object)(drWithValue("object")), [Is].TypeOf(GetType(Object)))
+			Assert.That(ConvertByCtype(Of Object)(drWithoutValue("object")), [Is].Null)
 
-			Assert.That(ConvertTo(Of Integer)(drWithValue("integer")), [Is].TypeOf(GetType(Integer)))
-			Assert.That(ConvertTo(Of Integer)(drWithoutValue("integer")), [Is].TypeOf(GetType(Integer)))
+			Assert.That(ConvertByCtype(Of Integer)(drWithValue("integer")), [Is].TypeOf(GetType(Integer)))
+			Assert.That(ConvertByCtype(Of Integer)(drWithoutValue("integer")), [Is].TypeOf(GetType(Integer)))
 
 		End Sub
 
-		Shared Function ConvertTo(Of T)(ByVal value As Object) As T
+		Shared Function ConvertByCtype(Of T)(ByVal value As Object) As T
 			If TypeOf (value) Is DBNull Then
 				Return CType(Nothing, T)
 			End If
@@ -45,6 +45,48 @@ Namespace SystemData
 			Catch ex As InvalidCastException
 				Return CType(Nothing, T)
 			End Try
+		End Function
+
+		<Test> Public Sub DataRow_Type_With_DefaultValue_Conversion_When_Null()
+
+			Dim drWithValue As DataRow = GetDataRow(True)
+			Dim drWithoutValue As DataRow = GetDataRow(False)
+
+			Assert.That(Convert_With_DefaultValue(Of String)(drWithValue("string")), [Is].TypeOf(GetType(String)))
+			Assert.That(Convert_With_DefaultValue(Of String)(drWithoutValue("string")), [Is].Null)
+
+			Assert.That(Convert_With_DefaultValue(Of Object)(drWithValue("object")), [Is].TypeOf(GetType(Object)))
+			Assert.That(Convert_With_DefaultValue(Of Object)(drWithoutValue("object")), [Is].Null)
+
+			Assert.That(Convert_With_DefaultValue(Of Integer)(drWithValue("integer")), [Is].TypeOf(GetType(Integer)))
+			Assert.That(Convert_With_DefaultValue(Of Integer)(drWithoutValue("integer")), [Is].TypeOf(GetType(Integer)))
+
+		End Sub
+
+		Shared Function Convert_With_DefaultValue(Of T)(ByVal value As Object) As T
+
+			If TypeOf (value) Is DBNull Then
+				Return GetDefaultValue(Of T)()
+			End If
+
+			Try
+				Return CType(value, T)
+			Catch ex As InvalidCastException
+				Return GetDefaultValue(Of T)()
+			End Try
+
+		End Function
+
+		Shared Function GetDefaultValue(Of T)() As T
+
+			Dim retVal As T = Nothing
+
+			If GetType(T).IsValueType Then
+				retVal = Activator.CreateInstance(GetType(T))
+			End If
+
+			Return retVal
+
 		End Function
 
 #Region "Infrastructure"
