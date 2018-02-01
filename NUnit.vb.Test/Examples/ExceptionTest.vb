09ef03d76573da595aedca3ace78723e45284483
f15ijp <f15ijp@users.noreply.github.com>
2018-02-01 02:35:32 +0100

    Adds more examples of Exceptions When function/sub takes argument

--- NUnit.vb.Test/Examples/ExceptionTest.vb
+++ NUnit.vb.Test/Examples/ExceptionTest.vb
@@ -1,6 +1,5 @@
 ﻿Option Explicit On
 
-Imports System
 Imports NUnit.Framework
 
 Namespace Examples
@@ -17,6 +16,15 @@ Namespace Examples
 		Public Sub Excpetion_Assert_That_TestSub()
 			Assert.That(AddressOf SubThatThrows, Throws.ArgumentException)
 		End Sub
+		<Test>
+		Public Sub Excpetion_Assert_Throws_SubWithArgument()
+			Assert.Throws(Of ArgumentException)(Sub() SubThatThrowsWithArgument(True))
+		End Sub
+
+		<Test>
+		Public Sub Excpetion_Assert_That_SubWithArgument()
+			Assert.That(Sub() SubThatThrowsWithArgument(True), Throws.ArgumentException)
+		End Sub
 
 		<Test>
 		Public Sub Excpetion_Assert_Throws_Function()
@@ -28,13 +36,41 @@ Namespace Examples
 			Assert.That(AddressOf FunctionThatThrows, Throws.ArgumentException)
 		End Sub
 
+		<Test>
+		Public Sub Excpetion_Assert_Throws_FunctionWithArgument()
+			Assert.Throws(Of ArgumentException)(Function() FunctionThatThrowsWithArgument(True))
+		End Sub
+
+		<Test>
+		Public Sub Excpetion_Assert_That_FunctionWithArgument()
+			Assert.That(Sub() FunctionThatThrowsWithArgument(True), Throws.ArgumentException)
+		End Sub
+
+#Region "Infrasctructure for test"
+
 		Private Shared Sub SubThatThrows()
 			Throw New ArgumentException("Throws for test")
+		End Sub
+
+		Private Shared Sub SubThatThrowsWithArgument(ByVal shouldThrow As Boolean)
+			If shouldThrow Then
+				Throw New ArgumentException("Throws for test")
+			End If
 		End Sub
 
 		Private Shared Function FunctionThatThrows()
 			Throw New ArgumentException("Throws for test")
 		End Function
+
+		Private Shared Function FunctionThatThrowsWithArgument(ByVal shouldThrow As Boolean)
+			If shouldThrow Then
+				Throw New ArgumentException("Throws for test")
+			Else
+				Return Nothing
+			End If
+		End Function
+
+#End Region
 
 	End Class
 
