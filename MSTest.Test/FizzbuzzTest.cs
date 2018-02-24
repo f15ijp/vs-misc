78fb52e18c8360b585e567b97399ad1569f9665e
Jonas <jonas@f15ijp.com>
2018-02-24 19:50:59 +0100

    Better naming for the Fizzbuzz tests

---
+++ MSTest.Test/FizzbuzzTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTest.Test
+{
+	[TestClass]
+	public class MSTest_Fizzbuzz
+	{
+		[TestMethod]
+		public void TestNegativeNumberThrowsEx()
+		{
+			Exception expectedException = null;
+			try
+			{
+				Fizzbuzz.FizzBuzz(-1);
+			}
+			catch(Exception ex)
+			{
+				expectedException = ex;
+			}
+			Assert.IsInstanceOfType(expectedException, typeof(ArgumentException));
+		}
+	}
+}
