af2ac228cfd4cfa718171768f4b0b0d40edb0625
Jonas <jonas@f15ijp.com>
2018-02-24 11:42:42 +0100

    Adds example for testing Fizzbuzz in vb as well

---
+++ NUnit.vb.Test/FizzbuzzTest.vb
@@ -0,0 +1,27 @@
+﻿Option Explicit On
+
+Imports NUnit.Framework
+
+<TestFixture>
+Public Class FizzbuzzTest
+
+	<TestCase(1, ExpectedResult:="1")>
+	<TestCase(2, ExpectedResult:="2")>
+	<TestCase(3, ExpectedResult:="Fizz")>
+	<TestCase(5, ExpectedResult:="Buzz")>
+	<TestCase(15, ExpectedResult:="FizzBuzz")>
+	Public Function TestCases(Input As Integer) As String
+		Return Fizzbuzz.FizzBuzz(Input)
+	End Function
+
+	<TestCase(-1)>
+	Public Sub TestExceptions_Assert_Throws(input As Integer)
+		Assert.Throws(Of ArgumentException)(Sub() Fizzbuzz.FizzBuzz(input))
+	End Sub
+
+	<TestCase(-1)>
+	Public Sub TestException_Assert_That(input As Integer)
+		Assert.That(Sub() Fizzbuzz.FizzBuzz(input), Throws.ArgumentException)
+	End Sub
+
+End Class
