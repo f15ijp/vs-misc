3bfcc44b2fc2c8b91c78573d76d197c653317d6c
Jonas <jonas@f15ijp.com>
2018-03-06 19:38:28 +0100

    Adds example of double.TryParse on en-US & sv-SE

---
+++ Examples.vb/ConversionExamples/DoubleTryParse.vb
@@ -0,0 +1,29 @@
+﻿Option Explicit On
+
+Imports System
+Imports NUnit.Framework
+
+Namespace Examples.vb
+
+	<TestFixture>
+	Public Class DoubleTryParse
+
+		<Test> <SetCulture("en-US")>
+		Public Sub DoubleTryParseExample_enUS()
+
+			Assert.That(Double.TryParse("1,5", Nothing), [Is].True)
+			Assert.That(Double.TryParse("1.5", Nothing), [Is].True)
+
+		End Sub
+
+		<Test> <SetCulture("sv-SE")>
+		Public Sub DoubleTryParseExample_svSE()
+
+			Assert.That(Double.TryParse("1,5", Nothing), [Is].True)
+			Assert.That(Double.TryParse("1.5", Nothing), [Is].False)
+
+		End Sub
+
+	End Class
+
+End Namespace
