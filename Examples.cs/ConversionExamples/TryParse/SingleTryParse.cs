1fe79cfd2acc1c3b37c91a91f64e60e5b5baa328
Jonas <jonas@f15ijp.com>
2018-03-06 19:40:07 +0100

    Adds example of single.TryParse en-US en-UK sv-SE

---
+++ Examples.cs/ConversionExamples/TryParse/SingleTryParse.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionExamples.TryParse
+{
+	[TestFixture]
+	public class SingleTryParse
+	{
+		[Test]
+		[SetCulture("en-US")]
+		public void SingleTryParseExample_enUS()
+		{
+			Assert.That(Single.TryParse("1,5", out float testVar), Is.True);
+			Assert.That(Single.TryParse("1.5", out testVar), Is.True);			
+		}
+
+		[Test]
+		[SetCulture("en-UK")]
+		public void SingleTryParseExample_enUK()
+		{
+			Assert.That(Single.TryParse("1,5", out float testVar), Is.True);
+			Assert.That(Single.TryParse("1.5", out testVar), Is.True);
+		}
+
+		[Test]
+		[SetCulture("sv-SE")]
+		public void SingleTryParseExample_svSE()
+		{
+			Assert.That(Single.TryParse("1,5", out float testVar), Is.True);
+			Assert.That(Single.TryParse("1.5", out testVar), Is.False);
+		}
+
+	}
+}
