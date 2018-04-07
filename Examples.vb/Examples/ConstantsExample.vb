182958f395cf0a39cfd6dc7616ef4b404b8bdc18
Jonas <jonas@f15ijp.com>
2018-04-07 17:56:39 +0200

    Adds ecample of how to change a constant in a inherited class

---
+++ Examples.vb/Examples/ConstantsExample.vb
@@ -0,0 +1,20 @@
+﻿Imports NUnit.Framework
+
+Namespace Examples
+	<TestFixture>
+	Public Class ConstantsExample
+		Public Const PUBLIC_CONST As Integer = 42
+		Private Const PRIVATE_CONST As Integer = 42
+
+		<Test> Public Sub ShadowsConstGetsOtherValue()
+			Assert.That(PUBLIC_CONST, [Is].Not.EqualTo(ClassThatInherits.PUBLIC_CONST))
+		End Sub
+
+	End Class
+
+	Public Class ClassThatInherits
+		Inherits ConstantsExample
+
+		Public Shadows Const PUBLIC_CONST = 4
+	End Class
+End Namespace
