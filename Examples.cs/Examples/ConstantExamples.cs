54e8f12dae43ff2c3dde8cc666f7feba589637c3
Jonas <jonas@f15ijp.com>
2018-04-08 17:00:10 +0200

    Example of chaging a const in a inherited class

---
+++ Examples.cs/Examples/ConstantExamples.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Examples.cs.Examples
+{
+	[TestFixture]
+	public class TestInheritedConstants
+	{
+		[Test]public void InheritedClassCanGiveConstNewValue()
+		{
+			Assert.That(InheritedClass.CONSTANTVALUE, Is.Not.EqualTo(BaseClass.CONSTANTVALUE));
+		}
+	}
+
+	public class BaseClass
+	{
+		public const int CONSTANTVALUE = 42;
+	}
+
+	public class InheritedClass:BaseClass
+	{
+		public new const int CONSTANTVALUE = 314;
+	}
+
+}
