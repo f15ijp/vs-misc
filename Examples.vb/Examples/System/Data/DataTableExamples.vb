9ebe5c5eae56a8432bfbf59174a58be39e236567
f15ijp <jonas.petersson@miljodata.se>
2018-04-15 20:16:32 +0200

    Simple example of how to loop a colums in a DataTable

---
+++ Examples.vb/Examples/System/Data/DataTableExamples.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Data
+
+Namespace Examples.System.Data
+	Public Class DataTableExamples
+		Public Sub LoopAllRowsAndColumnsInTable(ByVal dt As DataTable) 
+			For Each dr As DataRow In dt.Rows
+				Dim row As New List(Of KeyValuePair(Of String, String))
+				For Each dc As DataColumn In dt.Columns
+					row.Add(New KeyValuePair(Of String, String)(dc.ColumnName, dr(dc)))
+				Next
+			Next
+		End Sub
+	End Class
+End Namespace
