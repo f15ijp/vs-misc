ab94b77343c33e147af7a6aa616dd5a9ff2a10cf
f15ijp <jonas.petersson@miljodata.se>
2018-04-15 19:34:00 +0200

    Select by type equivalent for dot net 3.5

--- Examples.vb/Statements/SelectCaseExamples.vb
+++ Examples.vb/Statements/SelectCaseExamples.vb
@@ -9,23 +9,34 @@ Namespace Statements
 
 		<Test>
 		Public Sub Select_Case_Types()
-
 			Assert.That(GetTypeName(Of String), [Is].EqualTo("string"))
 			Assert.That(GetTypeName(Of Integer), [Is].EqualTo("integer"))
-			
 		End Sub
 
 		Function GetTypeName(Of T)() As String
-			
 			Select Case GetType(T)
 				Case GetType(String)
 					Return "string"
 				Case GetType(Integer)
-					return "integer"
+					Return "integer"
 			End Select
 			Throw New ArgumentOutOfRangeException
 		End Function
 
-	End Class
+		<Test>
+		Public Sub Test_GetTypeName_WorksOnDotNet3_5()
+			Assert.That(SelectCaseByTypeExuvalient_WorksOnDotNet35(String.Empty), [Is].EqualTo("string"))
+			Assert.That(SelectCaseByTypeExuvalient_WorksOnDotNet35(42), [Is].EqualTo("integer"))
+		End Sub
 
+		Function SelectCaseByTypeExuvalient_WorksOnDotNet35(ByVal input) As String
+			If TypeOf input Is String Then
+				Return "string"
+			ElseIf TypeOf input Is Integer Then
+				Return "integer"
+			End If
+			Throw New ArgumentOutOfRangeException
+		End Function
+
+	End Class
 End Namespace
