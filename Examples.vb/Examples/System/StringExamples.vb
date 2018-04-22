634cfe5d0609341de7662054be256d8ac35a2a5d
f15ijp <jonas.petersson@miljodata.se>
2018-04-22 11:22:32 +0200

    Some string.Split examples (index or FirstOrDefault)

---
+++ Examples.vb/Examples/System/StringExamples.vb
@@ -0,0 +1,24 @@
+﻿Imports NUnit.Framework
+
+Namespace Examples.System
+	<TestFixture()>
+	Public Class StringExamples
+		
+		<TestCase("")>
+		<TestCase(",")>
+		<TestCase("a,b")>
+		Public Sub Split_FirstOrDefault(ByVal input As String)
+			dim testVar As String = input.Split(",").FirstOrDefault()
+			Assert.That(testVar, [Is].TypeOf(GetType(String)))
+		End Sub
+
+		<TestCase("")>
+		<TestCase(",")>
+		<TestCase("a,b")>
+		Public Sub Split_ByIndex(ByVal input As String)
+			dim testVar As String = input.Split(",")(0)
+			Assert.That(testVar, [Is].TypeOf(GetType(String)))
+		End Sub
+
+	End Class
+End Namespace
