d284a44076a6b8e5fec7c973bf5575209fc0c870
f15ijp <jonas.petersson@miljodata.se>
2018-06-14 16:30:51 +0200

    Adds some Convert Examples bool toString and string to int32

---
+++ NUnit.vb.Test/Examples/System/ConvertExamples.vb
@@ -0,0 +1,25 @@
+﻿Imports NUnit.Framework
+
+Namespace Examples.System
+	<TestFixture()>
+	Public Class ConvertExamples
+
+		<Test>
+		Public Sub Convert_String_ToInt32_Throws()
+			Assert.Multiple(Sub()
+								Assert.Throws(Of FormatException)(Sub() Convert.ToInt32(String.Empty))
+								Assert.Throws(Of FormatException)(Sub() Convert.ToInt32(Guid.NewGuid().ToString()))
+							End Sub)
+
+		End Sub
+
+		<Test>
+		Public Sub Convert_ToString_Boolean()
+			Assert.Multiple(Sub()
+								Assert.That(Convert.ToString(True), [Is].EqualTo("True"))
+								Assert.That(Convert.ToString(False), [Is].EqualTo("False"))
+							End Sub)
+		End Sub
+
+	End Class
+End Namespace
