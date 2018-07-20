096a479d02be4bf74c8bdc289a451b7da0f82d1a
f15ijp <jonas.petersson@miljodata.se>
2018-07-20 21:09:23 +0200

    Adds example of how to join lines

---
+++ Examples.vb/Examples/JoinedLines.vb
@@ -0,0 +1,18 @@
+﻿Namespace Examples
+
+	Public Class JoinedLines
+		Public Sub JoinLinedExample()
+			Dim a As Integer, b As Integer, c As Integer
+
+			If a > 10 Then a = a + 1 : b = b + a : c = c + b
+
+			'Is equal to
+			If a > 10 Then
+				a = a + 1
+				b = b + a
+				c = c + b
+			End If
+
+		End Sub
+	End Class
+End Namespace
