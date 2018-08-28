d3c592c9925beba4bedd95ecaa9af86a8066ccc1
f15ijp <jonas.petersson@miljodata.se>
2018-08-28 21:27:00 +0200

    Adds example of how to loop though a enum
    (and moves the file to better location)

---
+++ Nunit.Test/Examples/System/EnumExamples.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Examples
+{
+	[TestFixture]
+	public class EnumExamples
+	{
+		public enum TestEnum
+		{
+			One,
+			Two
+		}
+
+		[Test]
+		public void LoopThroughEnum()
+		{
+			foreach (TestEnum testEnum in (TestEnum[])Enum.GetValues(typeof(TestEnum)))
+			{
+				//do something with the enum
+				Assert.That(testEnum, Is.Not.Null);
+			}
+		}
+
+	}
+}
