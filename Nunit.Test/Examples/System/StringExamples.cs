dde92cb271d4c8a0ef1b7524065644574da26938
f15ijp <jonas.petersson@miljodata.se>
2018-10-13 10:27:49 +0200

    Adds example of how to repeat a string

---
+++ Nunit.Test/Examples/System/StringExamples.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Nunit.Test.Examples.System
+{
+	[TestFixture]
+	public class StringExamples
+	{
+		[Test]
+		public void RepeatString()
+		{
+			Assert.Multiple(() =>
+			{
+				Assert.That(string.Concat(Enumerable.Repeat("a", 2)), Is.EqualTo("aa"));
+				Assert.That(string.Concat(Enumerable.Repeat("abab", 3)), Is.EqualTo("ababab"));
+			});
+		}
+
+	}
+}
