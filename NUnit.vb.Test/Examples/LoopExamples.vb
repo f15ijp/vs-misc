da441b300dd25a26b6c891fd92d00693a7285fc3
f15ijp <jonas.petersson@miljodata.se>
2018-10-13 11:13:27 +0200

    What happens when a loop goes from 0 to 0

---
+++ NUnit.vb.Test/Examples/LoopExamples.vb
@@ -0,0 +1,25 @@
+﻿Imports NUnit.Framework
+
+Namespace Examples
+	<TestFixture()>
+	Public Class LoopExamples
+		<Test>
+		Public Sub ForLoop_from_zero_to_one_minus_one()
+			Dim counter As Integer = 0
+			For i As Integer = 0 To 1 - 1
+				counter += 1
+			Next
+			Assert.That(counter, [Is].EqualTo(1))
+		End Sub
+
+		<Test>
+		Public Sub ForLoop_from_zero_to_zero()
+			Dim counter As Integer = 0
+			For i As Integer = 0 To 0
+				counter += 1
+			Next
+			Assert.That(counter, [Is].EqualTo(1))
+		End Sub
+
+	End Class
+End Namespace
