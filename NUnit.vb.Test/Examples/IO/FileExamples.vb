25038550c52f0f87214cee63417f4349512e7de3
f15ijp <jonas.petersson@miljodata.se>
2018-10-27 10:14:00 +0200

    Adds example of how to write a byte-array to a File

--- NUnit.vb.Test/Examples/IO/FileExamples.vb
+++ NUnit.vb.Test/Examples/IO/FileExamples.vb
@@ -1,8 +1,13 @@
-﻿Imports NUnit.Framework
+﻿Imports System.IO
+Imports NUnit.Framework
 
 Namespace Examples.IO
 	<TestFixture()>
 	Public Class FileExamples
+
+		Public Sub WriteByteArrayToFile(array As Byte(), fileName As String)
+			File.WriteAllBytes(fileName, array)
+		End Sub
 
 	End Class
 End Namespace
