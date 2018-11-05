217c56cd587ee4abd83cced5377ad868fa9db6fa
f15ijp <jonas.petersson@miljodata.se>
2018-11-05 10:22:00 +0100

    More DateDiff examples

--- NUnit.vb.Test/Examples/System/DateAndTimeExamples.vb
+++ NUnit.vb.Test/Examples/System/DateAndTimeExamples.vb
@@ -1,18 +1,21 @@
-﻿Imports NUnit.Framework
+Imports NUnit.Framework
 
 Namespace Examples.System
 	<TestFixture()>
 	Public Class DateAndTimeExamples
 
-		<TestCase("2018-01-01", "2018-01-02", ExpectedResult := 1)>
-		<TestCase("2018-01-01", "2018-01-02T01:02:03", ExpectedResult := 1)>
-		<TestCase("2018-01-01T01:02:03", "2018-01-02", ExpectedResult := 0)>
-		<TestCase("2018-10-31T01:02:03", "2018-11-01", ExpectedResult := 0)>
-		<TestCase("2018-10-31T01:02:03", "2018-11-01T01:02:02", ExpectedResult := 0)>
-		<TestCase("2018-10-31T01:02:03", "2018-11-01T01:02:03", ExpectedResult := 1)>
-		<TestCase("2018-10-31T01:02:03", "2018-11-01T01:02:04", ExpectedResult := 1)>
-		public Function Example_of_datediff_with_days(dateFrom As String, dateTo As String)
+		<TestCase("2018-01-01", "2018-01-02", ExpectedResult:=1)>
+		<TestCase("2018-01-01", "2018-01-02T01:02:03", ExpectedResult:=1)>
+		<TestCase("2018-01-01T01:02:03", "2018-01-02", ExpectedResult:=0)>
+		<TestCase("2018-10-31T01:02:03", "2018-11-01", ExpectedResult:=0)>
+		<TestCase("2018-10-31T01:02:03", "2018-11-01T01:02:02", ExpectedResult:=0)>
+		<TestCase("2018-10-31T01:02:03", "2018-11-01T00:00:00", ExpectedResult:=0)>
+		<TestCase("2018-10-31T01:02:03", "2018-11-01T23:59:00", ExpectedResult:=1)>
+		<TestCase("2018-10-31T01:02:03", "2018-11-01T01:02:03", ExpectedResult:=1)>
+		<TestCase("2018-10-31T01:02:03", "2018-11-01T01:02:04", ExpectedResult:=1)>
+		Public Function Example_of_datediff_with_days(dateFrom As String, dateTo As String)
 			Return DateDiff(DateInterval.Day, DateTime.Parse(dateFrom), DateTime.Parse(dateTo))
 		End Function
+
 	End Class
 End Namespace
