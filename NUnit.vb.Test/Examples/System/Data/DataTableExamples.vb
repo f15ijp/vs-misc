c0559085024d032043d75966d4bcc9439f16732a
f15ijp <jonas.petersson@miljodata.se>
2018-11-11 12:24:02 +0100

    Adds example of what happens when accessing a non existing row in a DT

--- NUnit.vb.Test/Examples/System/Data/DataTableExamples.vb
+++ NUnit.vb.Test/Examples/System/Data/DataTableExamples.vb
@@ -1,6 +1,8 @@
 Imports System.Data
+Imports NUnit.Framework
 
 Namespace Examples.System.Data
+	<TestFixture()>
 	Public Class DataTableExamples
 		Public Sub LoopAllRowsAndColumnsInTable(ByVal dt As DataTable) 
 			For Each dr As DataRow In dt.Rows
@@ -10,6 +12,15 @@ Namespace Examples.System.Data
 				Next
 			Next
 		End Sub
+		<Test()>
+		<Test()>
+		Public Sub Accessing_non_existant_row_by_index_gives_indexOutOfRangeExcetion()
+			Dim dt As DataTable = GetDataTable()
+			Assert.Throws(Of IndexOutOfRangeException)(Function()
+															Return dt.Rows(0)
+														End Function)
+		End Sub
+
 #Region "Infrastructure"
 
 		Public Shared Function GetDataTable() As DataTable
