0464600f21410eea8cc77d4cc8050dee13cea1ab
f15ijp <jonas.petersson@miljodata.se>
2018-12-01 17:40:42 +0100

    Comparing zero with empty string gives InvalidCastException

---
+++ NUnit.vb.Test/Examples/System/IntegerExamples.vb
@@ -0,0 +1,15 @@
+﻿Imports NUnit.Framework
+
+Namespace Examples.System
+	<TestFixture()>
+	Public Class IntegerExamples
+
+		<Test()>
+		Public Sub Compare_Zero_with_empty_string()
+			Assert.Throws(Of InvalidCastException)(Function()
+													   Return 0 = ""
+												   End Function)
+		End Sub
+
+	End Class
+End Namespace
