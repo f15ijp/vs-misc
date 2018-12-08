7682f981f1d103ad6c37479c4023d1d5de92d390
f15ijp <jonas.petersson@miljodata.se>
2018-12-08 12:16:49 +0100

    Example of how to use string.LastIndexOf
    Without warnings about it being culture specific.

--- Nunit.Test/Examples/System/StringExamples.cs
+++ Nunit.Test/Examples/System/StringExamples.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 using System;
 using System.Linq;
 
@@ -14,6 +14,14 @@ namespace Examples.System
 			{
 				Assert.That(string.Concat(Enumerable.Repeat("a", 2)), Is.EqualTo("aa"));
 				Assert.That(string.Concat(Enumerable.Repeat("abab", 3)), Is.EqualTo("abababababab"));
+			});
+		}
+
+		[TestCase("a.b.c.d", ".", 5)]
+		public void LastIndoxOf_without_warning(string haystack, string needle, int lastIndex){
+			Assert.Multiple(() => {
+				Assert.That(haystack.LastIndexOf(needle, StringComparison.Ordinal), Is.EqualTo(lastIndex));
+				Assert.That(haystack.LastIndexOf(needle, StringComparison.OrdinalIgnoreCase), Is.EqualTo(lastIndex));
 			});
 		}
 
