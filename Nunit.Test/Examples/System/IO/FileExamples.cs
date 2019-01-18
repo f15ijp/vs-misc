81a678b5d9231f8dfc850e59e44cfc11049f08ce
f15ijp <jonas.petersson@miljodata.se>
2019-01-18 12:23:22 +0100

    Adding exmple of how to write a file with UTF-8 BOM

--- Nunit.Test/Examples/System/IO/FileExamples.cs
+++ Nunit.Test/Examples/System/IO/FileExamples.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.IO;
+using System.Text;
+using NUnit.Framework;
 
 
 namespace Nunit.Test.Examples.System.IO
@@ -6,6 +9,15 @@ namespace Nunit.Test.Examples.System.IO
 	[TestFixture]
 	class FileExamples
 	{
+		[TestCase(true)]
+		[TestCase(false)]
+		public void WriteTextToFile_Utf8(bool writeByteOrderMarkToFile)
+		{
+			var tmpFileName = Path.GetTempFileName();
+			var textToWrite = Guid.NewGuid().ToString();
+			var theEncoding = new UTF8Encoding(writeByteOrderMarkToFile);
 
+			File.AppendAllText(tmpFileName, textToWrite, theEncoding);
+		}
 	}
 }
