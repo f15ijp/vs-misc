ff139cb860f752f9d5ff7c2d9b7fedb0c5935ee1
f15ijp <jonas.petersson@miljodata.se>
2019-01-19 09:15:00 +0100

    Simple sample to verify what ToString on a bool does

---
+++ NUnit.vb.Test/Examples/System/BooleanExamples.vb
@@ -0,0 +1,15 @@
+﻿Imports NUnit.Framework
+
+Namespace Examples.System
+	<TestFixture()>
+	Public Class BooleanExamples
+
+		<Test>
+		Public Sub ToStringGives()
+			Assert.Multiple(Sub()
+								Assert.That(True.ToString, [Is].EqualTo("True"))
+								Assert.That(False.ToString, [Is].EqualTo("False"))
+							End Sub)
+		End Sub
+	End Class
+End Namespace
---
+++ Nunit.Test/Examples/System/BoolExamples.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Examples.Examples.System
+{
+	[TestFixture]
+	public class BoolExamples
+	{
+		[Test]
+		public void ToStringGives()
+		{
+			Assert.Multiple(() =>
+			{
+				Assert.That(true.ToString(), Is.EqualTo("True"));
+				Assert.That(false.ToString(), Is.EqualTo("False"));
+			});
+		}
+	}
+}
