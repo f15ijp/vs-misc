b3ebbe8cba6619a54330eea129a3e910973bd98c
f15ijp <jonas.petersson@miljodata.se>
2019-01-19 16:53:22 +0100

    Using DirectoryInfo and FileInfo to list files in a directory

---
+++ Nunit.Test/Examples/System/IO/DirectoryInfoAndFileInfoExamples.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Examples.Examples.System.IO {
+	public class DirectoryInfoAndFileInfoExamples {
+
+		[Test]
+		public void ListFilesInDirectory() {
+			var di = new DirectoryInfo(Environment.CurrentDirectory);
+			TestContext.WriteLine($"Listing files from {di.FullName}");
+			foreach (var myFileInfo in di.GetFiles()) {
+				TestContext.WriteLine(myFileInfo.Name);
+			}
+		}
+
+	}
+}
