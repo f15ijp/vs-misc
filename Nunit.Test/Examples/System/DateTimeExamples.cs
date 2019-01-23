6d59c80da3002bc26a39d4f4cf3c875c0eb9281d
f15ijp <jonas.petersson@miljodata.se>
2019-01-23 20:15:53 +0100

    Examples of ToString with Culture on DateTime

--- Nunit.Test/Examples/System/DateTimeExamples.cs
+++ Nunit.Test/Examples/System/DateTimeExamples.cs
@@ -1,9 +1,30 @@
 ﻿using System;
+using System.Globalization;
 using NUnit.Framework;
 
-namespace Nunit.Test.Examples.System
+namespace Examples.Examples.System
 {
 	public class DateTimeExamples
 	{
+		[Test]
+		public void ToString_Examples()
+		{
+			var dateValue = DateTime.Now;
+			TestContext.WriteLine($"Without formating/culture [a.k.a. use current culture] {dateValue.ToString()}");
+
+			CultureInfo[] cultures = {
+				new CultureInfo("en-US"),
+				new CultureInfo("fr-FR"),
+				new CultureInfo("it-IT"),
+				new CultureInfo("de-DE"),
+				new CultureInfo("sv-SE")
+		};
+			foreach (var culture in cultures)
+			{
+				TestContext.WriteLine("Using culture {0}: {1}", culture.Name, dateValue.ToString(culture));
+			}
+
+
+		}
 	}
 }
