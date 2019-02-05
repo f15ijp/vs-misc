a762b0cb5bfddf17c5f05aba745ad3cb28e14019
f15ijp <jonas.petersson@miljodata.se>
2019-02-05 20:26:14 +0100

    Adds example of how to declare and assign array & Dictionary

---
+++ Nunit.Test/Examples/System/Collections/ArrayExamples.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Examples.Examples.System.Collections
+{
+	[TestFixture]
+	public class ArrayExamples
+	{
+		[Test]
+		public void StringArrayDeclaredWithValues() {
+			var myArray = new[] { "a", "b" };
+			Assert.That(myArray.Length, Is.GreaterThan(1));
+			Assert.That(myArray, Is.TypeOf<string[]>());
+		}
+
+		[Test]
+		public void IntArrayDeclaredWithValues() {
+			var myArray = new [] { 1, 2, 3 };
+			Assert.That(myArray.Length, Is.GreaterThan(1));
+			Assert.That(myArray, Is.TypeOf<int[]>());
+		}
+	}
+}
---
+++ Nunit.Test/Examples/System/Collections/Generics/DictionaryExamples.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Examples.Examples.System.Collections.Generics
+{
+	[TestFixture]
+	public class DictionaryExamples
+	{
+
+		[Test]
+		public void CreateAndAssign()
+		{
+			var myDict = new Dictionary<int, string>
+			{
+				{1, Guid.NewGuid().ToString()},
+				{2, Guid.NewGuid().ToString()}
+			};
+
+			Assert.That(myDict.Count, Is.GreaterThan(0));
+		}
+
+		[Test]
+		public void CreateAndAssignWithClassInitializer() {
+			var myDict = new Dictionary<int, StudentName>
+			{
+				{1, new StudentName { FirstName="Sachin", LastName="Karnik", ID=211 }},
+				{2, new StudentName { FirstName="Dina", LastName="Salimzianova", ID=317 }}
+			};
+
+			Assert.That(myDict.Count, Is.GreaterThan(0));
+		}
+
+		protected class StudentName {
+			public string FirstName { get; set; }
+			public string LastName { get; set; }
+			public int ID { get; set; }
+		}
+
+	}
+}
