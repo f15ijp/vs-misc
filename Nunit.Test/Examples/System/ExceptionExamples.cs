6503d62c9d9e5e300a59f6cb46638ef91061e9d9
f15ijp <jonas.petersson@miljodata.se>
2019-03-03 11:33:04 +0100

    Adds example of how to count the number of ex in a loop

---
+++ Nunit.Test/Examples/System/ExceptionExamples.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Examples.Examples.System {
+	[TestFixture]
+	public class ExceptionExamples {
+		[Test]
+		public void ExceptionInLoopKeepOnRunning()
+		{
+			var loopedLines = 0;
+			var linesWithException = 0;
+			for (var counter = 0; counter < 10; counter++)
+			{
+				try 
+				{
+					ThrowIfModIsZero(counter, 5);
+				}
+				catch (Exception)
+				{
+					linesWithException++;
+				}
+
+				loopedLines++;
+			}
+			TestContext.WriteLine($"Total: {loopedLines.ToString()} number with exception: {linesWithException.ToString()}");
+		}
+
+		public void ThrowIfModIsZero(int number, int mod)
+		{
+			if (number % mod == 0)
+			{
+				throw new Exception();
+			}
+		}
+
+	}
+}
