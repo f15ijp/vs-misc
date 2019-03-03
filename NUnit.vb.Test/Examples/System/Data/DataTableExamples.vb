b29ba85f9ca0cc892a474a9195f53aef79a94d60
f15ijp <jonas.petersson@miljodata.se>
2019-03-03 12:01:49 +0100

    Adds more examples with DataTable
    Accessing no existing row.
    Removing rows by index.
    Removing row without index.

--- NUnit.vb.Test/Examples/System/Data/DataTableExamples.vb
+++ NUnit.vb.Test/Examples/System/Data/DataTableExamples.vb
@@ -21,6 +21,34 @@ Namespace Examples.System.Data
 		End Sub
 
 		<Test()>
+		Public Sub Removing_a_row_from_an_DataTable()
+			Dim dt As DataTable = GetDataTable(noOfRows:=1)
+
+			dt.Rows.RemoveAt(0)
+			Assert.That(dt.Rows.Count, [Is].EqualTo(0))
+		End Sub
+
+		<Test()>
+		Public Sub Removing_rows_from_position_zero_of_an_DataTable()
+			Dim dt As DataTable = GetDataTable(noOfRows:=5)
+
+			For i As Integer = 0 To dt.Rows.Count -1
+				dt.Rows.RemoveAt(0)
+			Next
+			Assert.That(dt.Rows.Count, [Is].EqualTo(0))
+		End Sub
+
+		Private Sub DoSomethingWithADataRow(ByVal dr As DataRow)
+		End Sub
+		<Test()>
+		Public Sub Accessing_non_existant_row_by_index_gives_indexOutOfRangeExcetion_when_calling_other_function()
+			Dim dt As DataTable = GetDataTable()
+			Assert.Throws(Of IndexOutOfRangeException)(Sub()
+															DoSomethingWithADataRow(dt.Rows(0))
+														End Sub)
+		End Sub
+
+		<Test()>
 		Public Sub Accessing_non_existant_row_by_index_gives_indexOutOfRangeExcetion()
 			Dim dt As DataTable = GetDataTable()
 			Assert.Throws(Of IndexOutOfRangeException)(Function()
@@ -28,9 +56,17 @@ Namespace Examples.System.Data
 														End Function)
 		End Sub
 
+		<Test()>
+		Public Sub Accessing_column_in_non_existant_row_by_index_gives_indexOutOfRangeExcetion()
+			Dim dt As DataTable = GetDataTable()
+			Assert.Throws(Of IndexOutOfRangeException)(Function()
+															Return dt.Rows(0)("string")
+														End Function)
+		End Sub
+
 #Region "Infrastructure"
 
-		Public Shared Function GetDataTable() As DataTable
+		Public Shared Function GetDataTable(Optional ByVal noOfRows As Integer = 0) As DataTable
 
 			Dim dt As New DataTable()
 			dt.Columns.Add(New DataColumn("string", GetType(String)))
@@ -39,6 +75,10 @@ Namespace Examples.System.Data
 			dt.Columns.Add(New DataColumn("null", GetType(Object)))
 
 			dt.AcceptChanges()
+			For i As Integer = 0 To noOfRows-1
+				dt.Rows.InsertAt(dt.NewRow(), 0)
+			Next
+
 			Return dt
 
 		End Function
