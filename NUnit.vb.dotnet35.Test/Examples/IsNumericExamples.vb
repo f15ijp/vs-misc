b6c38f65e57e691ad7f6c3f90544957cb8191481
f15ijp <jonas.petersson@miljodata.se>
2019-07-06 08:45:51 +0200

    Adds examples of IsNumeric

---
+++ NUnit.vb.dotnet35.Test/Examples/IsNumericExamples.vb
@@ -0,0 +1,21 @@
+﻿Option Explicit On
+
+Imports System
+Imports NUnit.Framework
+
+Namespace NUnit.vb.dotnet35.Test
+
+	<TestFixture>
+	Public Class IsNumericExamples
+
+		<TestCase("1.0.0", true)>
+		<TestCase("1.0,0", true)>
+		<TestCase("1,0.0", false)>
+		<TestCase("1,0,0", false)>
+		Public Sub TestMethod(input As String, expected As Boolean)
+			Assert.That(IsNumeric(input), [Is].EqualTo(expected))
+		End Sub
+
+	End Class
+
+End Namespace
