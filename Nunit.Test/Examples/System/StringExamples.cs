55867f888f879c1a6714c1fb637a01c37db87d86
f15ijp <jonas.petersson@miljodata.se>
2019-07-06 10:38:27 +0200

    Adds example of Trim (and variants of Trim)

--- Nunit.Test/Examples/System/StringExamples.cs
+++ Nunit.Test/Examples/System/StringExamples.cs
@@ -14,11 +14,25 @@ namespace Examples.Examples.System {
 		}
 
 		[TestCase("a.b.c.d", ".", 5)]
-		public void LastIndoxOf_without_warning(string haystack, string needle, int lastIndex){
+		public void LastIndexOf_without_warning(string haystack, string needle, int lastIndex) {
 			Assert.Multiple(() => {
 				Assert.That(haystack.LastIndexOf(needle, StringComparison.Ordinal), Is.EqualTo(lastIndex));
 				Assert.That(haystack.LastIndexOf(needle, StringComparison.OrdinalIgnoreCase), Is.EqualTo(lastIndex));
 			});
+		}
+
+		[Test]
+		public void DifferenceBetweenTrims() {
+			var stringWithSpace = " hello ";
+
+			Assert.Multiple(() =>
+			{
+				Assert.That(stringWithSpace.Trim(), Does.Not.Contain(" "));
+				Assert.That(stringWithSpace.TrimEnd(), Does.Not.EndWith(" "));
+				Assert.That(stringWithSpace.TrimStart(), Does.Not.StartWith(" "));
+			});
+			
+
 		}
 
 	}
