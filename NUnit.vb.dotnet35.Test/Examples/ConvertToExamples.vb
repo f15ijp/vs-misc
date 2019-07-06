ec2d5e6faf53f44b5d843e62b7871dafce99e040
f15ijp <jonas.petersson@miljodata.se>
2019-07-06 09:36:33 +0200

    Adds example of Convert.ToDouble

---
+++ NUnit.vb.dotnet35.Test/Examples/ConvertToExamples.vb
@@ -0,0 +1,25 @@
+﻿Option Explicit On
+
+Imports NUnit.Framework
+
+Namespace Examples
+
+	<TestFixture>
+	Public Class ConvertToExamples
+
+		<SetCulture("en-US")>
+		<TestCase("1,0", true)>
+		<TestCase("1.0", true)>
+		<TestCase("1.0.0", false)>
+		<TestCase("", false)>
+		Public Sub ToDouble(input As String, expected As Boolean)
+			If expected Then
+				Assert.DoesNotThrow(Sub() Convert.ToDouble(input))
+			Else
+				Assert.Throws(of FormatException)(Sub() Convert.ToDouble(input))
+			End If
+		End Sub
+
+	End Class
+
+End Namespace
