dcabc32e33fc09c90ff0c7c72b7c317b4a589722
f15ijp <jonas.petersson@miljodata.se>
2019-07-07 11:34:36 +0200

    Examples of how to convert list to a comma-separated string

--- Nunit.Test/Examples/System/Collections/Generics/ListExample.cs
+++ Nunit.Test/Examples/System/Collections/Generics/ListExample.cs
@@ -14,7 +14,34 @@ namespace Examples.Examples.System.Collections.Generics {
 				Assert.That(list1.Count, Is.EqualTo(3));
 				Assert.That(list2.Count, Is.EqualTo(4));
 			});
-
 		}
+
+		[Test]
+		public void List_of_int_to_commaseparated_string()
+		{
+			var list = new[] { 1, 2, 3 }.ToList<int>();
+
+			//If dotnet 3.5
+			var commaseparated35 = string.Join(",", list.Select(i => i.ToString()).ToArray());
+			//If dotnet 4+
+			var commasepareted4 = string.Join(",", list);
+			TestContext.Out.WriteLine(commaseparated35);
+
+			Assert.That(commaseparated35, Is.EqualTo(commasepareted4));
+		}
+
+		[Test]
+		public void List_of_string_to_commaseparated_string() {
+			var list = new[] { "1", "2", "3" }.ToList<string>();
+
+			//If dotnet 3.5
+			var commaseparated35 = string.Join(",", list.Select(i => i).ToArray());
+			//If dotnet 4+
+			var commasepareted4 = string.Join(",", list);
+			TestContext.Out.WriteLine(commaseparated35);
+
+			Assert.That(commaseparated35, Is.EqualTo(commasepareted4));
+		}
+
 	}
 }
