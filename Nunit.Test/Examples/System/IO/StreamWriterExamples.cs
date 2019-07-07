e1a07a97a488de99924cda6fe051ff6e8746156a
f15ijp <jonas.petersson@miljodata.se>
2019-07-07 11:14:21 +0200

    Adds StreamWriterExample

---
+++ Nunit.Test/Examples/System/IO/StreamWriterExamples.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Examples.Examples.System.IO {
+	[TestFixture]
+	public class StreamWriterExamples {
+		[TestCase(true)]
+		[TestCase(false)]
+		public void AppendToFile(bool append) {
+			var filePath = Path.GetTempFileName();
+			try {
+				using (var sw = new StreamWriter(filePath, append)) {
+					//somehow write something...
+					sw.WriteLine(Guid.NewGuid().ToString());
+				}
+			} finally {
+				if (File.Exists(filePath)) {
+					File.Delete(filePath);
+				}
+			}
+		}
+	}
+}
