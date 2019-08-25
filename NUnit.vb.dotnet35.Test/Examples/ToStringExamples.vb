d37116c2d673da0a3804ddf449d5f769b64d669d
f15ijp <jonas.petersson@miljodata.se>
2019-08-25 09:51:07 +0200

    Adds example of Boolean.ToString

---
+++ NUnit.vb.dotnet35.Test/Examples/ToStringExamples.vb
@@ -0,0 +1,18 @@
+﻿Option Explicit On
+
+Imports NUnit.Framework
+
+Namespace Examples
+
+	<TestFixture>
+	Public Class ToStringExamples
+
+		<Test>
+		Public Sub Boolean_ToString(<Values>theBool as Boolean)
+			TestContext.WriteLine(theBool.ToString())
+			Assert.That(theBool.ToString(), [Is].Not.Empty)
+		End Sub
+
+	End Class
+
+End Namespace
