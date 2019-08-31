e578e87ddeb8e33476df80a5b74edef8b21dcd13
f15ijp <jonas.petersson@miljodata.se>
2019-08-31 11:50:37 +0200

    Adds example of how to check if a list is empty

---
+++ Nunit.Test/TestExamples/ListTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Examples.TestExamples {
+	[TestFixture]
+	public class ListTest {
+		[Test]
+		public void EmptyList() {
+			Assert.Multiple(() => {
+				var myList = new List<object>();
+				Assert.That(myList.Count, Is.Zero);
+				Assert.That(myList, Is.Null.Or.Empty);
+			});
+		}
+	}
+}
