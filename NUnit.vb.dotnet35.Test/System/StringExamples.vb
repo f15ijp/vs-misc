62b80aa09c95a56d170167e45b0da86bcbf34473
f15ijp <jonas.petersson@miljodata.se>
2019-11-24 10:58:31 +0100

    Example of adding nothing to string

---
+++ NUnit.vb.dotnet35.Test/System/StringExamples.vb
@@ -0,0 +1,24 @@
+﻿Option Explicit On
+
+Imports NUnit.Framework
+
+
+Namespace System
+
+	<TestFixture>
+	Public Class StringExamples
+
+		<Test>
+		Public Sub TestAddingNothingToString()
+			Assert.Multiple(Sub()
+								Assert.That((String.Empty + Nothing), [Is].TypeOf(Of String), " + null")
+								Assert.That((String.Empty & Nothing), [Is].TypeOf(Of String), "& null")
+								Assert.Throws(Of ArgumentNullException)(Sub()
+																		String.Empty.Concat(Nothing)
+																	End Sub, "Concat null gives exception")
+							End Sub)
+		End Sub
+
+	End Class
+
+End Namespace
