b04313924e0b19dc13f80d2daf039e5f74aafbf5
f15ijp <jonas.petersson@miljodata.se>
2019-11-24 10:58:00 +0100

    Correct spelling of Exception and additional When example

--- NUnit.vb.Test/Examples/System/ExceptionExamples.vb
+++ NUnit.vb.Test/Examples/System/ExceptionExamples.vb
@@ -3,14 +3,16 @@ Imports NUnit.Framework
 
 Namespace Examples.System
 
-	Public Class ExecptionExamples
+	Public Class ExceptionExamples
 
 		<Test>
-		Public Sub CatchExeptionWith()
+		Public Sub CatchExceptionWhen()
 			Try
 				'Do some webrequest ...
 			Catch ex As WebException When ex.Status = WebExceptionStatus.ConnectionClosed OrElse ex.Status = WebExceptionStatus.KeepAliveFailure
-
+				'ConnectionClosed or KeepAliveFailure
+			Catch ex As WebException When ex.Status = WebExceptionStatus.ConnectFailure
+				'ConnectFailure
 			End Try
 		End Sub
 
