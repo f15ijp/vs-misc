a08670558d346d62ce3d69da7c7ef6ceec94844d
f15ijp <jonas.petersson@miljodata.se>
2020-01-02 11:00:05 +0100

    Adds example of how to use TestCase and T

--- Nunit.Test/TestExamples/TypeOfTest.cs
+++ Nunit.Test/TestExamples/TypeOfTest.cs
@@ -1,19 +1,28 @@
 ﻿using System.Collections.Generic;
 using NUnit.Framework;
 
-namespace Examples.TestExamples
-{
+namespace Examples.TestExamples {
 	[TestFixture]
-	public class TypeOfTest
-	{
+	public class TypeOfTest {
 		[Test]
-		public void Is_TypeOf_ListOfInt()
-		{
+		public void Is_TypeOf_ListOfInt() {
 			var theList = new List<int>();
 
 			Assert.That(theList, Is.TypeOf<List<int>>());
 
 			TestContext.WriteLine($"{theList.GetType()}");
+		}
+
+		[TestCase(0)]
+		[TestCase(0D)]
+		[TestCase(0F)]
+		[TestCase("")]
+		public void TestCase_of_t<T>(T input) {
+			if (typeof(T) == typeof(string)) {
+				TestContext.Write("String found");
+			}
+
+			TestContext.WriteLine($"The type of input is '{typeof(T)}'");
 		}
 	}
 }
