10e550834ac65aee9fe1488d9429bc150b3d53f7
f15ijp <jonas.petersson@miljodata.se>
2020-10-30 09:57:41 +0100

    Adding example of how to test with fixed culture
    Examples of both seting a culture on a test and
    how to run the same test with multiple cultures .

---
+++ Nunit.Test/TestExamples/CultureTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Examples.TestExamples {
+	public class CultureTest {
+
+		[Test]
+		[SetCulture("en-US")]
+		public void SingleCultreUsingSetCulture_USA() {
+			var date = new DateTime(2012, 10, 14);
+			var dateString = date.ToString("dd/MM/yyyy");
+			Assert.That(dateString, Is.EqualTo("14/10/2012"));
+		}
+
+		[Test]
+		[SetCulture("sv-SE")]
+		public void SingleCultreUsingSetCulture_Sweden() {
+			var date = new DateTime(2012, 10, 14);
+			var dateString = date.ToString("dd/MM/yyyy");
+			Assert.That(dateString, Is.EqualTo("14-10-2012"));
+		}
+
+		[Test]
+		[TestCase("en-US")]
+		[TestCase("sv-SE")]
+		[TestCase("da-DK")]
+		public void MultipleCulturesUsingTestCases(string cultureName) {
+			var culture = CultureInfo.GetCultureInfo(cultureName);
+			Thread.CurrentThread.CurrentCulture = culture;
+			Thread.CurrentThread.CurrentUICulture = culture;
+
+			var date = new DateTime(2012, 10, 14);
+			var dateString = date.ToString("dd/MM/yyyy");
+			//This is a bad test, it is only here to show that the culture is different 
+			Assert.That(dateString, cultureName == "en-US" ? Is.EqualTo("14/10/2012") : Is.EqualTo("14-10-2012"));
+		}
+	}
+}
