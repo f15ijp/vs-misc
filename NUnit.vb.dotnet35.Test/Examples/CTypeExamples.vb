6ffc408a7068c65e18ca17f4c2f3fac4c07c5396
f15ijp <jonas.petersson@miljodata.se>
2020-11-14 14:14:14 +0100

    Adding examples of CType

---
+++ NUnit.vb.dotnet35.Test/Examples/CTypeExamples.vb
@@ -0,0 +1,12 @@
+﻿Imports NUnit.Framework
+
+Namespace Examples
+	Public Class CTypeExamples
+
+		<Test>
+		Public Sub CInt_Nohthing()
+			Assert.That(CInt(Nothing), [Is].Zero)
+		End Sub
+
+	End Class
+End Namespace
