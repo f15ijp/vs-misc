d0d6972b34fa08e235c38ec090b9b61876b222db
f15ijp <jonas.petersson@miljodata.se>
2020-11-28 15:15:15 +0100

    Checking out how to HTML encode

---
+++ Nunit.Test/Examples/System/Web/HtmlEncodeExamples.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using NUnit.Framework;
+
+namespace Examples.Examples.System.Web {
+	public class HtmlEncodeExamples {
+
+		[TestCase("hello<p>")]
+		public void HtmlEncodeText(string input) {
+			var encodedText = HttpUtility.HtmlEncode(input);
+			TestContext.WriteLine($"input: '{input}'");
+			TestContext.WriteLine($"result: '{encodedText}'");
+		}
+
+		[Test]
+		public void HtmlEnocdeNull() {
+			// ReSharper disable ExpressionIsAlwaysNull
+			string input = null;
+			// ReSharper disable once AssignNullToNotNullAttribute
+			var encodedText = HttpUtility.HtmlEncode(s: input);
+			Assert.That(encodedText, Is.Null);
+			// ReSharper restore ExpressionIsAlwaysNull
+		}
+
+
+	}
+}
