2b022d3e93d48236bbc51916a96a5d1a2700b640
f15ijp <jonas.petersson@miljodata.se>
2020-11-30 10:45:36 +0100

    Testing how to invert a number

---
+++ Nunit.Test/Examples/NumberExamples.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+
+namespace Examples.Examples {
+	public class NumberExamples {
+
+		[TestCase(42)]
+		public void MultiplyByMinusOne_InvertsTheValue(int input)
+		{
+			Assert.Multiple(() =>
+			{
+				Assert.That(input * -1, Is.Negative);
+				Assert.That(input * -1, Is.EqualTo(-input));
+				Assert.That(-input * -1, Is.Positive);
+				Assert.That(-input * -1, Is.EqualTo(input));
+			});
+		}
+
+	}
+}
