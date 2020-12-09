ca180dc3397210ed60cbeadf17eba8e42119668c
f15ijp <f15ijp@users.noreply.github.com>
2020-12-09 20:11:12 +0100

    Accessing an index not in the array
    Checking out what happens (witch type of
    exception is thrown) when accessing
    and index that is not inside the array.

---
+++ NUnit.vb.Test/Examples/ArrayExamples.vb
@@ -0,0 +1,20 @@
+﻿Imports NUnit.Framework
+
+Namespace Examples
+
+	<TestFixture>
+	Public Class ArrayExamples
+
+		<Test>
+		Public Sub CallingIndexOutsideArrayGivesIndexOutOfRangeException()
+			Dim theArray = {String.Empty, String.Empty}
+
+			Assert.Throws(Of IndexOutOfRangeException)(Sub()
+														   Dim test = theArray(3)
+													   End Sub)
+
+		End Sub
+
+	End Class
+
+End Namespace
