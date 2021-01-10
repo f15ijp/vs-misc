42441a344d064e2c44e6be2c99385ba95f149403
f15ijp <f15ijp@users.noreply.github.com>
2021-01-10 11:25:23 +0100

    Check if properties as ByRef or ByVal
    A class to test how ByRef and ByVal works, what is what etc.

---
+++ Nunit.Test/Examples/ByRefOrByVal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Examples.Examples {
+	[TestFixture]
+	public class ByRefOrByVal {
+
+		[Test]
+		public void PropertiesGivesValuesNotReferences() {
+			var test = new TestObject();
+
+			var before = test.Id;
+			ChangeId(test);
+			var after = test.Id;
+
+			Assert.That(before, Is.Not.EqualTo(after));
+
+		}
+
+		private void ChangeId(TestObject input) {
+			input.Id = -input.Id + 5;
+		}
+
+		public class TestObject {
+			public int Id { get; set; }
+			public Guid Guid { get; set; }
+
+			public TestObject() {
+				Id = DateTime.Now.Millisecond + 1;
+				Guid = new Guid();
+			}
+		}
+
+	}
+}
